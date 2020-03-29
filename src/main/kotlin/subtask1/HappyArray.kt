02d6f48a72d91465a5fa8705f86eb486d3d12d1c
ykhudaleyeu <57769163+ykhudaleyeu@users.noreply.github.com>
2020-03-29 22:54:19 +0300

    update : HappyArray implementation

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,31 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
-    fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var array = sadArray.toCollection(ArrayList())
+        var arrayLast = ArrayList<Int>()
+        var result = true
+
+        while (result) {
+            result = false
+            for ((index, value) in array.withIndex()) {
+                if (index == 0 || index == array.lastIndex) {
+                    arrayLast.add(value)
+                }
+                if (index !== 0 && index < array.lastIndex) {
+                    var prevVal = array.get(index - 1)
+                    var nextVal = array.get(index + 1)
+                    if (value < prevVal + nextVal) {
+                        arrayLast.add(value)
+                    } else if (value > prevVal + nextVal) {
+                        result = true
+                    }
+                }
+            }
+            if (result) {
+                array = arrayLast
+                arrayLast = ArrayList()
+            }
+        }
+        return arrayLast.toIntArray()
     }
 }
