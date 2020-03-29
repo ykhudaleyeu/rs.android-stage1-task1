b58cef942091ada501fba1e95e4c6c36c4d68146
ykhudaleyeu <57769163+ykhudaleyeu@users.noreply.github.com>
2020-03-29 22:58:52 +0300

    update : StringParser implementation

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,24 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
-    fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+    var arr = ArrayList<String>()
+        val openSymbol = "<[("
+        val closedSymbol = ">])"
+        var inputLenght = inputString.length
+        var count = 0
+        inputString.indices.forEach { i ->
+            var tmp = ""
+            var value = inputString[i]
+            if (openSymbol.indexOf(value) >= 0) {
+                for (i in i + 1 until inputLenght) {
+                    if (count == 0 && inputString[i] == closedSymbol[openSymbol.indexOf(value)]) break
+                    if (inputString[i] == openSymbol[openSymbol.indexOf(value)]) ++count
+                    if (inputString[i] == closedSymbol[openSymbol.indexOf(value)]) --count
+                    tmp += inputString[i].toString()
+                }
+                arr.add(tmp)
+            }
+        }
+        return arr.toTypedArray()
     }
 }
